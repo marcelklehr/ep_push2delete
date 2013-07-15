c4e39df7e954a926fa4fed0f30faf7df2c9d6d3b
vileda <vileda@piratenfraktion-nrw.de>
2013-07-15 12:23:35 +0200

    stupid typo

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ exports.handleMessage = function(hook_name, context, callback){
 }
 
 exports.eejsBlock_editbarMenuRight = function(hook_name, args, cb) {
-  if(!args.renderContext.req.match(/^\/(p\/r\..{16})/)) {
+  if(!args.renderContext.req.url.match(/^\/(p\/r\..{16})/)) {
     args.content = eejs.require('ep_push2delete/templates/delete_button.ejs') + args.content;
   }
   cb();
